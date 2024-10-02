f7e6da2a278e73ef323a1c0df1e14ac4307f11b5
basmalahesham <112768821+basmalahesham@users.noreply.github.com>
2024-10-02 21:41:02 +0300

    add DecorationImage & appBar

--- lib/layout/home_layout_view.dart
+++ lib/layout/home_layout_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class HomeLayoutView extends StatefulWidget {
   const HomeLayoutView({super.key});
@@ -12,8 +13,28 @@ class HomeLayoutView extends StatefulWidget {
 class _HomeLayoutViewState extends State<HomeLayoutView> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Container(),
+    return Container(
+      decoration: const BoxDecoration(
+        color: Colors.white,
+        image: DecorationImage(
+          image: AssetImage(
+            'assets/images/pattern.png',
+          ),
+          fit: BoxFit.cover,
+        ),
+      ),
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text(
+            'News App',
+            style: GoogleFonts.exo(
+              fontSize: 22,
+              fontWeight: FontWeight.w500,
+            ),
+          ),
+        ),
+        body: Container(),
+      ),
     );
   }
 }
