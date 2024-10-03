b8b8275190e83c86013237bae7c2a8461ddb492c
basmalahesham <112768821+basmalahesham@users.noreply.github.com>
2024-10-03 23:49:03 +0300

    add GridView in home_layout_view.dart create grid_view_item_builder.dart

--- lib/layout/home_layout_view.dart
+++ lib/layout/home_layout_view.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:news_app/layout/widgets/custom_drawer.dart';
+import 'package:news_app/layout/widgets/grid_view_item_builder.dart';
 import 'package:news_app/models/category_model.dart';
 
 class HomeLayoutView extends StatefulWidget {
@@ -79,7 +80,39 @@ class _HomeLayoutViewState extends State<HomeLayoutView> {
           ),
         ),
         drawer: const CustomDrawer(),
-        body: Container(),
+        body: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                'Pick your Category \n of interest',
+                textAlign: TextAlign.start,
+                style: GoogleFonts.poppins(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 22,
+                  color: const Color(0xFF4F5A69),
+                ),
+              ),
+              Expanded(
+                child: GridView.builder(
+                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 2,
+                    mainAxisSpacing: 15,
+                    crossAxisSpacing: 15,
+                    childAspectRatio: 7 / 8,
+                  ),
+                  itemBuilder: (context, index) => GridViewItemBuilder(
+                    categoryModel: categoriesList[index],
+                    index: index,
+                    onClicked: () {},
+                  ),
+                  itemCount: categoriesList.length,
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
