b5a99854f496b9794993740d980959867ffe8416
basmalahesham <112768821+basmalahesham@users.noreply.github.com>
2024-10-04 20:25:01 +0300

    create articals_model.dart

---
+++ lib/models/articals_model.dart
@@ -0,0 +1,107 @@
+/// status : "ok"
+/// totalResults : 10
+/// articles : [{"source":{"id":"bbc-news","name":"BBC News"},"author":"BBC News","title":"Next Mission: Impossible film delayed by a year by US actors' strike","description":"The eighth movie in the hit Tom Cruise franchise won't now be released until May 2025.","url":"http://www.bbc.co.uk/news/entertainment-arts-67203951","urlToImage":"https://ichef.bbci.co.uk/news/1024/branded_news/B0C3/production/_131515254_gettyimages-1535082031.jpg","publishedAt":"2023-10-24T16:37:25.0676545Z","content":"The latest Mission: Impossible film has been delayed by almost a year after production was halted because of the ongoing Hollywood actors' strike.\r\nThe eighth film in the franchise will now be releas… [+2489 chars]"},{"source":{"id":"bbc-news","name":"BBC News"},"author":"BBC News","title":"Trump fraud trial live updates: Michael Cohen testifies","description":"Donald Trump is in court to hear testimony from his former \"fixer\", Michael Cohen.","url":"http://www.bbc.co.uk/news/live/world-us-canada-67199656","urlToImage":"https://m.files.bbci.co.uk/modules/bbc-morph-news-waf-page-meta/5.3.0/bbc_news_logo.png","publishedAt":"2023-10-24T16:37:24.0835248Z","content":"Trump is sitting in court now flanked by lawyersImage caption: Trump is sitting in court now flanked by lawyers\r\nBefore Michael Cohen takes the stand, let's dig into how Donald Trump got to this poin… [+957 chars]"},{"source":{"id":"bbc-news","name":"BBC News"},"author":"BBC News","title":"Car workers strike costing GM $200m a week","description":"The UAW action over pay and benefits is now in its sixth week and involves more than 40,000 people.","url":"http://www.bbc.co.uk/news/business-67207828","urlToImage":"https://ichef.bbci.co.uk/news/1024/branded_news/16A09/production/_131518629_whatsubject.jpg","publishedAt":"2023-10-24T15:37:22.92641Z","content":"A strike by car workers in the US is costing General Motors roughly $200m (£164m) each week, the firm has said. \r\nThe carmaker revealed the hit as it released its latest results.\r\nA row over pay and … [+2427 chars]"},{"source":{"id":"bbc-news","name":"BBC News"},"author":"BBC News","title":"Ukraine war: Avdiivka civilians cling on amid Russian assault","description":"Just over 1,000 civilians remain in Avdiivka, focus of a bloody Russian assault in eastern Ukraine.","url":"http://www.bbc.co.uk/news/world-europe-67196921","urlToImage":"https://ichef.bbci.co.uk/news/1024/branded_news/2C4D/production/_131514311_8eb464e00ae49bcd51b01b52d052dd79cf1d25b6.jpg","publishedAt":"2023-10-24T15:37:20.5207587Z","content":"As Russian missiles shatter the town of Avdiivka, Hanna is desperate to escape. But her elderly mother refuses to leave. \r\n\"I can't leave her,\" Hanna tells us over the phone. \"She said she wanted to … [+3416 chars]"},{"source":{"id":"bbc-news","name":"BBC News"},"author":"BBC News","title":"'Failure to act' on suicide website linked to 50 UK deaths","description":"Families are angry at the lack of action to shut it, despite multiple concerns from police and coroners.","url":"http://www.bbc.co.uk/news/uk-67082224","urlToImage":"https://ichef.bbci.co.uk/news/1024/branded_news/5F7F/production/_131474442_suicide_forum_index_promo_976-nc-34.png","publishedAt":"2023-10-24T15:07:25.459102Z","content":"British authorities failed to act on multiple official warnings about a website promoting suicide that has been connected to at least 50 UK deaths, the BBC has found. \r\nThe online forum, which we are… [+10546 chars]"},{"source":{"id":"bbc-news","name":"BBC News"},"author":"BBC News","title":"US politician Jeff Wilson arrested on Hong Kong gun charge","description":"The state senator appeared in court after carrying the handgun in his luggage on a flight from San Francisco.","url":"http://www.bbc.co.uk/news/world-asia-67204397","urlToImage":"https://ichef.bbci.co.uk/news/1024/branded_news/14475/production/_131516038_jeffwilson3fb.jpg","publishedAt":"2023-10-24T15:07:23.6298233Z","content":"A US politician has been arrested in Hong Kong for having a gun in his carry-on luggage on a flight.\r\nJeff Wilson, a Republican senator from Washington state, was arrested at Hong Kong International … [+2096 chars]"},{"source":{"id":"bbc-news","name":"BBC News"},"author":"BBC News","title":"Donald Trump looms large over House Speaker vote","description":"The former President has so far not publicly endorsed a new candidate after Jim Jordan dropped out.","url":"http://www.bbc.co.uk/news/world-us-canada-67208351","urlToImage":"https://ichef.bbci.co.uk/news/1024/branded_news/64B5/production/_131518752_gettyimages-1741629401-1.jpg","publishedAt":"2023-10-24T15:07:21.0367181Z","content":"The spectre of former President Donald Trump loomed large over the US Capitol on Tuesday as House Republicans met to choose a new Speaker candidate. \r\nWhile Mr Trump has not publicly endorsed a candi… [+2518 chars]"},{"source":{"id":"bbc-news","name":"BBC News"},"author":"BBC News","title":"Third Trump lawyer, Jenna Ellis, pleads guilty in Georgia subversion case","description":"Jenna Ellis is the third former lawyer to Donald Trump to plead guilty to election subversion.","url":"http://www.bbc.co.uk/news/world-us-canada-67208871","urlToImage":"https://ichef.bbci.co.uk/news/1024/branded_news/18561/production/_131518699_c8b5a1be267e4bc12b5ae2cf6a2313a4122dbc23.jpg","publishedAt":"2023-10-24T14:52:19.6772472Z","content":"A third former lawyer to Donald Trump has pleaded guilty in an election subversion case in the US state of Georgia.\r\nJenna Ellis is the fourth of 19 co-defendants to plead guilty in a deal with Fulto… [+918 chars]"},{"source":{"id":"bbc-news","name":"BBC News"},"author":"BBC News","title":"Seven dead after 'super fog' kills seven in New Orleans crash","description":"A dense fog caused a pile-up of more than 150 vehicles near New Orleans, killing seven and wounding 25.","url":"http://www.bbc.co.uk/news/world-us-canada-67204993","urlToImage":"https://ichef.bbci.co.uk/news/1024/branded_news/12DC3/production/_131515277_new-orleans-highway-lsp-a.jpg","publishedAt":"2023-10-24T10:52:23.5058468Z","content":"At least seven people have died after a dense \"super fog\" caused a huge, 158-car pile-up near New Orleans.\r\nThick fog and fumes from multiple marsh fires mingled to reduce visibility for drivers comm… [+1528 chars]"},{"source":{"id":"bbc-news","name":"BBC News"},"author":"BBC News","title":"The Rock waxwork museum working on 'urgent' skin tone fix","description":"Paris's Grevin museum promises to address concerns over the skin tone of its life-sized figure.","url":"http://www.bbc.co.uk/news/newsbeat-67203892","urlToImage":"https://ichef.bbci.co.uk/news/1024/branded_news/D99F/production/_131511755_gettyimages-1738672830.jpg","publishedAt":"2023-10-24T08:52:19.4487281Z","content":"A French museum is working urgently to fix a waxwork of Dwayne \"The Rock\" Johnson after he complained about it.\r\nThe Grevin Museum in Paris proudly revealed the life-sized figure of the wrestler-turn… [+1603 chars]"}]
+library;
+
+class ArticalsModel {
+  String? status;
+  List<Articles>? articles;
+
+  ArticalsModel({
+    this.status,
+    this.articles,
+  });
+
+  ArticalsModel.fromJson(dynamic json) {
+    status = json['status'];
+    if (json['articles'] != null) {
+      articles = [];
+      json['articles'].forEach((v) {
+        articles?.add(Articles.fromJson(v));
+      });
+    }
+  }
+}
+
+/// source : {"id":"bbc-news","name":"BBC News"}
+/// author : "BBC News"
+/// title : "Next Mission: Impossible film delayed by a year by US actors' strike"
+/// description : "The eighth movie in the hit Tom Cruise franchise won't now be released until May 2025."
+/// url : "http://www.bbc.co.uk/news/entertainment-arts-67203951"
+/// urlToImage : "https://ichef.bbci.co.uk/news/1024/branded_news/B0C3/production/_131515254_gettyimages-1535082031.jpg"
+/// publishedAt : "2023-10-24T16:37:25.0676545Z"
+/// content : "The latest Mission: Impossible film has been delayed by almost a year after production was halted because of the ongoing Hollywood actors' strike.\r\nThe eighth film in the franchise will now be releas… [+2489 chars]"
+
+class Articles {
+  SourceArtical? source;
+  String? author;
+  String? title;
+  String? description;
+  String? url;
+  String? urlToImage;
+  String? publishedAt;
+  String? content;
+
+  Articles({
+    this.source,
+    this.author,
+    this.title,
+    this.description,
+    this.url,
+    this.urlToImage,
+    this.publishedAt,
+    this.content,
+  });
+
+  Articles.fromJson(dynamic json) {
+    source =
+    json['source'] != null ? SourceArtical.fromJson(json['source']) : null;
+    author = json['author'];
+    title = json['title'];
+    description = json['description'];
+    url = json['url'];
+    urlToImage = json['urlToImage'];
+    publishedAt = json['publishedAt'];
+    content = json['content'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final map = <String, dynamic>{};
+    if (source != null) {
+      map['source'] = source?.toJson();
+    }
+    map['author'] = author;
+    map['title'] = title;
+    map['description'] = description;
+    map['url'] = url;
+    map['urlToImage'] = urlToImage;
+    map['publishedAt'] = publishedAt;
+    map['content'] = content;
+    return map;
+  }
+}
+
+/// id : "bbc-news"
+/// name : "BBC News"
+
+class SourceArtical {
+  SourceArtical({
+    this.id,
+    this.name,
+  });
+
+  SourceArtical.fromJson(dynamic json) {
+    id = json['id'];
+    name = json['name'];
+  }
+
+  String? id;
+  String? name;
+
+  Map<String, dynamic> toJson() {
+    final map = <String, dynamic>{};
+    map['id'] = id;
+    map['name'] = name;
+    return map;
+  }
+}
