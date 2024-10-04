644dccc9dbfb915656228f4346deb9660d96cfdf
basmalahesham <112768821+basmalahesham@users.noreply.github.com>
2024-10-04 20:22:43 +0300

    create fetchArticals function

--- lib/core/network/api_manager.dart
+++ lib/core/network/api_manager.dart
@@ -1,7 +1,9 @@
 import 'dart:convert';
+import 'dart:developer';
 
 import 'package:flutter/cupertino.dart';
 import 'package:http/http.dart' as http;
+import 'package:news_app/models/articals_model.dart';
 import 'package:news_app/models/source_model.dart';
 
 class ApiManager {
@@ -13,7 +15,10 @@ class ApiManager {
 
   static Future<SourceModel> fetchSources(String category) async {
     // query parameters
-    var query = {"apiKey": apiKey, "category": category};
+    var query = {
+      "apiKey": apiKey,
+      "category": category,
+    };
     final response = await http.get(
       Uri.https(
         baseUrl, // domain
@@ -30,5 +35,27 @@ class ApiManager {
     } else {
       throw Exception('Failed to load sources');
     }
+  }
+
+  static Future<ArticalsModel> fetchArticals(String sourceId) async {
+    Map<String, dynamic>? queryParameters = {
+      "apiKey": apiKey,
+      "sources": sourceId,
+    };
+
+    Uri uri = Uri.https(
+      baseUrl,
+      "/v2/top-headlines",
+      queryParameters,
+    );
+
+    var response = await http.get(uri);
+
+    ArticalsModel articalsModel =
+        ArticalsModel.fromJson(jsonDecode(response.body));
+
+    log(response.body);
+
+    return articalsModel;
   }
 }
